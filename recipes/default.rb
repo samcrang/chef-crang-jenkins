2ba1ac9d6d2d8265cfdd44d953a0aa7b07ab3d2a
Sam Crang <sam.crang@gmail.com>
2013-08-25 19:51:41 +0100

    Add default cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,10 @@
 # Cookbook Name:: crang-jenkins
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Sam Crang 
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe 'jenkins::server'
+include_recipe 'jenkins::proxy'
