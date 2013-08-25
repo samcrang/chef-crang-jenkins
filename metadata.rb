3579205e86b2f17fc4e08cc3766eb803aba5ae93
Sam Crang <sam.crang@gmail.com>
2013-08-25 19:23:49 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'crang-jenkins'
+maintainer       'Sam Crang'
+maintainer_email 'sam.crang@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures crang-jenkins'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
